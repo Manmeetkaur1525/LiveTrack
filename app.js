7ff6f9eb0a20d92c94bc8344804234af0d2a599e
Manmeetkaur1525 <kaurmanmeet1525@gmail.com>
2025-04-03 02:34:07 +0530

    feat:basic boiler plate for setting up dependencies

---
+++ app.js
@@ -0,0 +1,17 @@
+const  express = require ('express');
+const http = require("http")
+const app = express();
+
+const socketio = require("socket.io")
+const server = http.createServer(app)
+
+const io = socketio(server)
+
+//ejs
+
+app.set("view engine",ejs);
+app.set(express.static(path.join(__dirname,"public")));
+app.get('/' ,function(req,res){
+    res.send("hey")
+});
+server.listen(3001);
